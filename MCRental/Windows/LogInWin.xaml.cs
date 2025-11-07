e29b79eef770d691ebcf90b26b81116eb6fb69ca
MohamedAzahaf <mohamed.azahaf@student.hogent.be>
2025-11-07 23:54:14 +0100

    ghtr

--- MCRental/Windows/LogInWin.xaml.cs
+++ MCRental/Windows/LogInWin.xaml.cs
@@ -70,6 +70,7 @@ namespace MCRental_Client.Windows
                                 }
                                 App.MainWindow.btnLogout.Visibility = Visibility.Visible;
                                 App.MainWindow.mniProfiel.Visibility = Visibility.Visible;
+                                App.MainWindow.btnRegistreer.Visibility = Visibility.Visible;
                             }
                         }
                         tbError.Text = "Ongeldige username of wachtwoord.";
